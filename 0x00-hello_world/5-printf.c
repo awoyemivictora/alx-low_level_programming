8b2513437176b58eefe641e446dbab09c397916d
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-07 23:15:52 +0000

    My new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+#include <string.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
