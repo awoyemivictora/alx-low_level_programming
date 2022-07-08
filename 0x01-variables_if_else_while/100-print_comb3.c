b01d970eb664042d2dd4d698830a2cd9970a8612
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-08 11:58:38 +0000

    My new commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,30 +2,37 @@
 
 /**
  * main - Prints all possible different combinations of two digits
- *
- * Return: Always (Success)
+ * Numbers must be separated by , followed by a space
+ * The two digits must be different
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Return 0
  */
 
 int main(void)
 {
-	int c, i;
+	int n1 = 48;
+	int n2;
+	int com = 44;
 
-	for(c = '0'; i <= '9'; i++)
+	while (n1 <= 57)
 	{
-		for(i = '0'; i <= '9'; i++)
-		{
-			if(c < i)
-			{
-				putchar(c);
-				putchar(i);
+		n2 = n1 + 1;
 
-				if(c != '8' || (c == '8' && i != '9'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+		while (n2 <= 57)
+		{
+			putchar(n1);
+			putchar(n2);
+
+			if (n1 != 56 || n2 != 57)
+			{
+				putchar(com);
+				putchar(32);
 			}
+
+			n2 += 1;
 		}
+
+		n1 += 1;
 	}
 
 	putchar('\n');
