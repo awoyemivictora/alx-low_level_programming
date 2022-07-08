22fcf09dbb0d40d507bcd6fb4329e23d08b4a378
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-08 12:03:52 +0000

    My new commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,16 +2,18 @@
 
 /**
  * main - Prints all the numbers of base 16 in lowercase
- *
+ * Followed by a new line
+ * Return: 0
  */
 
 int main(void)
 {
+	int n;
 	char c;
 
-	for(c = '0'; c <= '9'; c++)
+	for (n = 48; n < 58'; n++)
 	{
-		putchar(c);
+		putchar(n);
 	}
 
 	for(c = 'a'' c <= 'f'; c++)
