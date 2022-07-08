24520c7da515226f9fac4ec8c2cc28f2eeeef498
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-08 15:46:34 +0000

    My new commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Function that checks for uppercase character
+ * @c: input value to check
+ * Return: 1 if uppercase character 0 if not
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
