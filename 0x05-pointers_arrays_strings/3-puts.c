7940af6b85497c4964cb96d8aab0cc4523d0890d
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-14 23:27:18 +0000

    My new commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- *  * _puts - prints a string
- *   * @str: string
- *    * Return: void
+ * _puts -> this is a function that puts
+ *
+ * @str: a param to _puts function
+ *
  */
-void _puts(char *str)
-{
-	int count = 0;
 
-	while (count >= 0)
+void _puts(char *str)
+
+{
+	for (; *str != '\0'; str++)
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		_putchar(*str);
 	}
+	_putchar('\n');
+
 }
