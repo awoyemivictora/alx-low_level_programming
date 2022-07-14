4463d7904496be843493e5cc013dd9e1649a2dd0
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-14 22:22:06 +0000

    My new commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - Function that changes all lowercase letters of a string to upppercase
+ *
+ * @x: string param
+ * Return: string
+ */
+
+char *string_toupper(char *);
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >+ 97 && x[i] <= 122)
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
+}
