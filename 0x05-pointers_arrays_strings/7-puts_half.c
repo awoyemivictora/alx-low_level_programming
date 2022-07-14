2be946fc02621044eb51796659298f0c66ca75b0
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-14 23:20:21 +0000

    My new commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * puts_half - puts half
+ * puts_half -> puts half
  * @str: string param
  */
 
@@ -13,7 +13,6 @@ void puts_half(char *str)
 	x = strlen(str);
 	if (x % 2 == 1)
 		y = x / 2 + 1;
-
 	else
 		y = x / 2;
 	for (i = y; i < x; i++)
