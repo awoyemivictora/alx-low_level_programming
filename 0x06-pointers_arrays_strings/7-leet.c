9e0cc29dbdd5471f4661159d9ccb17c647776495
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-14 22:35:05 +0000

    My new commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - Function that encodes a string into 1337.
+ * @s: Given string
+ * Return: codified string
+ */
+
+char *leet(char *s)
+{
+	char str1[] = "aAeEo0tTlL";
+	char str2[] = "4433007711";
+	int ia = 0;
+	int ib = 0;
+
+	while (s[ia] != '\0')
+	{
+		for (ib = 0; ib <= 10; ib++)
+		{
+			if (s[ia] == str1[ib])
+				s[ia] = str2[ib];
+		}
+		ia++;
+	}
+	return (s);
+}
