ebac348d5c4faa51b1eb97e510a4eb1842ea8c63
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-15 07:35:18 +0000

    My new commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,7 +8,6 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-  a[2] = 98;
   /*
    * write your line of code here...
    * Remember:
@@ -19,6 +18,7 @@ int main(void)
    */
   ;
   /* ...so that this prints 98\n */
+  *(p + 5) = 98;
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
