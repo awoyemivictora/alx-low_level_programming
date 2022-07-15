36916901aa8e073970202a281216aadcacb2e6a3
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-15 07:17:41 +0000

    My new commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
+void _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void puts(char *str);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
