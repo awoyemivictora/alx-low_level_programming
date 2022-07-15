54760e830ca72c7d6edf510d3ebd4bc0c80244dc
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-15 07:13:56 +0000

    My new commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,38 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * puts_half -> puts half
- * @str: string param
+ * puts_half - prints second half of a string
+ * @str: pointer to string
+ *
+ * Return: none
+ *
  */
-
 void puts_half(char *str)
 {
-	int x, y, i;
+	int i, length, half;
 
-	x = strlen(str);
-	if (x % 2 == 1)
-		y = x / 2 + 1;
-	else
-		y = x / 2;
-	for (i = y; i < x; i++)
-		_putchar(str[i]);
+	if (*str != '\0')
+	{
+		length = 0;
+		while (*(str + length) != '\0')
+		{
+			length++;
+		}
+		if (length % 2 == 0)
+			half = length / 2;
+		else
+			half = length - (length - 1) / 2;
+		i = half;
+		if (length == 1)
+			_putchar(*str);
+		else
+		{
+			while (*(str + i) != '\0')
+			{
+				_putchar(*(str + i));
+				i++;
+			}
+		}
+	}
 	_putchar('\n');
 }
