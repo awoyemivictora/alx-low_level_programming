05cb8069ad3e284c24fbc2101c8b30d47735716d
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-15 06:58:54 +0000

    My new commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,25 @@
 #include "main.h"
+
 /**
- * print_rev - check the code for Alx School students.
- * @s: pointer int type
- * Return: Always 0.
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: pointer to string
+ *
+ * Return: none
+ *
  */
 void print_rev(char *s)
 {
-int len;
-for (len = 0; *s != '\0'; len++)
-s++;
+	int i, count;
 
-s--;
-for (len = len; len != 0; len--)
-{
-_putchar(*s);
-s--;
-}
-
-_putchar('\n');
+	if (*s != '\0')
+	{
+		count = 0;
+		while (*(s + count) != '\0')
+		{
+			count++;
+		}
+		for (i = count - 1; i >= 0; i--)
+			_putchar(*(s + i));
+	}
+	_putchar('\n');
 }
