96c55e7aaa50ceb7fc1db941d8357da1b30a72f6
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-15 06:55:06 +0000

    My new commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
-#include "main.h"
+#include "main.c"
 
 /**
-* * reset_to_98 -> a function to update the reference of a pointer
-* * @n: pointer to n to be updated
-*
-* Return: 0
-*/
+ * reset_to_98 - changes pinter to point to value 98
+ * @n: pointer to n
+ *
+ * Return: none
+ *
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
