8b1d745b17c11f967cda11d6b937d0c4117344b4
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-15 06:48:28 +0000

    My new commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,9 +5,16 @@
  */
 void _puts(char *str)
 {
-for (; *str != '\0'; str++)
-{
-_putchar(*str);
-}
-_putchar('\n');
+	int i;
+
+	if (*str != '\0')
+	{
+		i = 0;
+		while (*(str + i) != '\0')
+		{
+			_putchar(*(str + i));
+			i++;
+		}
+	}
+	_putchar('\n');
 }
