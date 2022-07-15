db77cbc6fc9a09d9409965b8654520d69af0e8be
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-15 07:48:51 +0000

    My new commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_buffer - Function that prints a buffer
@@ -9,7 +10,40 @@
  * Return: string
  */
 
+int printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/**
+ * print_buffer - Function that prints a buffer
+ * @b: string
+ * @size: size of buffer
+ */
+
 void print_buffer(char *b, int size)
 {
+	int start, end;
 
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n');
+		}
+	}else
+		printf("\n");
 }
