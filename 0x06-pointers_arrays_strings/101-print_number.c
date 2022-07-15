c73c9ce4916c94bb67ba716b312b8140e591f6f6
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-15 07:31:57 +0000

    My new commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,16 +10,18 @@ void print_number(int n)
 {
 	unsigned int n1;
 
-	n1 = n;
-
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
 		n1 = -n;
 	}
-	if (n1 / 10 != 0)
+	else
 	{
-		print_number(n1 / 10);
+		n1 = n;
 	}
+	if (n1 / 10)
+		print_number(n1 / 10);
+
 	_putchar(n1 % 10 + '0');
 }
