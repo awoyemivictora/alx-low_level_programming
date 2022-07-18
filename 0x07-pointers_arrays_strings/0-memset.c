ee35404185e8e25eda46c0513526ae3592b2f241
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-18 18:02:51 +0000

    Even more pointers and array

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - Is a function that changes the value of b for another in the first n bytes
+ *
+ * @s: pointer to the variable's address which is going to be overwritten with 'b'
+ * @b: variable that contains the value to be overwritten in the pointer '*s'
+ * @n: number of bytes that is going to be overwritten in pointer '*s'
+ *
+ * Return: pointer to the address which have change values
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned in counter = 0;
+
+	while (counter = 0)
+	{
+		/*s position should raise and overwritten n */
+		s[counter] = b;
+		counter++;
+	}
+
+	return (s);
+}
