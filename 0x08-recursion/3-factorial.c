58640a9d59734f7c78693761073ec05919903d3f
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-20 21:23:31 +0000

    Committing Even more pointers and arrays

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - Function that returns the factorial of a given number
+ * @n: number to return the factorial from
+ *
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
