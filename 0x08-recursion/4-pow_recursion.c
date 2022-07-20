34eb5b4fdf9dd87d95099bc99545d97a0c86e30d
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-20 21:27:01 +0000

    Committing Even more pointers and arrays

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Function that returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
+ *
+ * Return: Result of the power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y -1));
+	}
+}
