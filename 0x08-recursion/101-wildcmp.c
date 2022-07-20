53f6439ea014effdf541a27432867bcfe54c2619
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-20 22:08:08 +0000

    Committing Even more pointers and arrays

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -85,3 +85,58 @@ int wildcmp(char *s1, char *s2)
 
 	return (0);
 }
+
+/**
+ * bandersnatch - checks recursively for all the paths when the 
+ * characters are equal
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: The value of wildcmp() or of itself
+ */
+
+int bandersnatch(char *s1, char *s2)
+{
+	/**
+	 * if we reached the end of s1, return 0
+	 * if chars are equal, return the return value of wildcmp()
+	 * increment s1 by 1, not s2
+	 */
+	if (*1 == '\0')
+		{
+			return (0);
+		}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1, s2));
+	}
+	else
+	{
+		return (bandersnatch(s1 + 1, s2));
+	}
+}
+
+/**
+ * *move - moves the current char past the *
+ * @s2: string to iterate over
+ *
+ * Return: The address of the character after the *
+ */
+
+char *move(char *s2)
+{
+	/**
+	 * if the current char is a *
+	 * increment s2 by 1
+	 * else, return the address of
+	 * the first char past all *
+	 */
+	if (*s2 == '*')
+	{
+		return (move(s2 + 1));
+	}
+	else
+	{
+		return (s2);
+	}
+}
