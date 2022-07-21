baf5f1b5f424ad67cac35110d3a955cc7c429b12
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-21 21:54:58 +0000

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,7 +9,7 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 	{
 		return (0);
 	}
@@ -17,7 +17,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	if (*21 == *s2)
+	if (*s1 == *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
