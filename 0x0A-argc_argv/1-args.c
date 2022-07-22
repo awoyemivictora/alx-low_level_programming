e7dd0e90c51eadb44e352e82b90903a691c109fd
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-22 18:22:01 +0000

    recursion

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program that prints the number of arguments passed into it
+ *
+ * Return: Nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+	
+	return 0;
+}
