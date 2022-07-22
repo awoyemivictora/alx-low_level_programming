13c6ec7c21e01fe70a69fbc4feff434d2daf2383
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-22 23:11:19 +0000

    recursion

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Function that prints its name followed by a new line
+ * main - prints its name followed by a new line
  * @argc - argument count
  * @arv: arguments
  *
