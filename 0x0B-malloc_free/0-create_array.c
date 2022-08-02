442ee88a50d7d782e679d034a9e317098c0e855e
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-08-02 19:55:55 +0000

    0x0A. C - argc, argv

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars
- * and initializes it with a specific char
- * @size: the size of the array
- * @c: initial value
- *
- * Return: a pointer to the array, or NULL if it fails
- */
+* create_array - creates an array of chars
+* and initializes it with a specific char
+* @size: the size of the array
+* @c: initial value
+*
+* Return: a pointer to the array, or NULL if it fails
+*/
 
  char *create_array(unsigned int size, char c)
  {
