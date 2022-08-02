81b42426f2d08103a8b6c96f3e9cb31308431f6e
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-08-02 22:04:55 +0000

    0x0A. C - argc, argv

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,15 +2,14 @@
 #define DOG_H
 
 /**
-* struct dog - structure definition of a dog
-* @name: character string
-* @age: integer
-* @owner: character string
-*/
-
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
 typedef struct dog
 {
-	char *name:
+	char *name;
 	float age;
 	char *owner;
 } dog_t;
@@ -19,6 +18,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
-
