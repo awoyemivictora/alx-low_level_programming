8828195e0fd660d981ef743fffa32d0ad3f3136f
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-08-08 22:57:08 +0000

    Creating some Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,17 +6,21 @@
  * @separator: the string to be printed between numbers
  * @n: count of parameters
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list list;
+	char *hold;
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(list, int));
-
+		hold = va_arg(list, char*);
+		if (hold == NULL)
+				printf("(nil)");
+		else
+				printf("%s", hold);
 		if (i != (n - 1) && separator != NULL)
 				printf("%s", separator);
 	}
