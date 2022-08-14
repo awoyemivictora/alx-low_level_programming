46b8cede177f826d7e83dd2dc9184d3e4980de4b
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-08-14 12:49:35 +0000

    Creating stingly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elements
- * @h: pointer
+ * print_list - print all element of a linked list
+ * @h: head of the linked list
  *
  * Return: number of nodes of the linked list
  */
@@ -14,9 +14,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-				printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
-				printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		count++;
 		h = h->next;
 	}
