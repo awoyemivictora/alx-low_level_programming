66e5a7e1ca008c4d3ba9b0aae99491412efb6cc9
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-08-15 23:47:02 +0000

    Creating stingly linked lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * add_node - add a node to the beginning of a linked list
+ * add_node - add a node the beginning of a linked list
  * @head: pointer reference to the head of the linked list
  * @str: pointer to the str to be added to the node
  *
- * Return: Address of the new element, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (!new_node)
-			return (NULL);
+		return (NULL);
 
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
