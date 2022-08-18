70f04c0f2565fa23f8d90f699898c40d90d82f77
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-08-18 21:31:08 +0000

    Working on bit manipulation and number systems in C

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
