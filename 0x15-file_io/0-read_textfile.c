4c48b745ad7088e37c56c99290e2f4b3bf1a65b9
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-08-23 01:26:56 +0000

    Creating Files I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,23 +3,24 @@
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file
- * @letters: The number of letters the function should read and print
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- * Otherwise - The actual number of bytes the function can read and print.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
+	 ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	if (buffer ==  NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
@@ -34,4 +35,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	free(buffer);
 	close(o);
+
+	return (w);
+
 }
